2f9a5534e8179ce039754b1281eb825fd4cedbd1
Gustavo <gustavobrandao31@outlook.com>
2021-10-07 18:48:33 -0300

    Programmers Art 1/2 & Ajustes

---
+++ funcoes.js
@@ -0,0 +1,17 @@
+var programmer_art = 0;
+
+function programmers_art(auto){
+
+    if(typeof auto == "undefined")
+        if(programmer_art == 0)
+            programmer_art = 1;
+        else
+            programmer_art = 0;
+    
+    if(programmer_art)
+        document.getElementById("programmers_art").style.border = "solid 1px yellow";
+    else
+        document.getElementById("programmers_art").style.border = "solid 1px black";
+
+    localStorage.setItem("programmer_art", programmer_art);
+}
